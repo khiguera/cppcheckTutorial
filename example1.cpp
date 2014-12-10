9a64c4a074c04030b4ed7805cfb6e40f328bb619
Leon Peng <lpeng003@hammer.cs.ucr.edu>
2014-12-10 04:31:20 -0800

    More examples

---
+++ example1.cpp
@@ -0,0 +1,8 @@
+int foo()
+{
+	char arr[10];
+	
+	arr[15] = 1;
+
+}
+
---
+++ example3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void greaterThanZero(int x)
+{
+	int i;
+	if(x > 0 )
+	{
+		std::cout << "X is greater than 0." << std::endl;
+		i = 1;
+	}
+}
+
+int foo(int x, int y)
+{
+	return x + y;
+}
+
+int main()
+{
+	int a = 1, b = 2;
+	foo(,b);
+
+	return 0;
+}
